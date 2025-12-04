40963afbe1251a30455e3d795114c4f476a4857b
uechan9220 <k.uejima0922@gmail.com>
2025-12-04 22:51:15 +0900

    aaab

--- src/bot.ts
+++ src/bot.ts
@@ -17,6 +17,7 @@ import { load } from "https://deno.land/std@0.208.0/dotenv/mod.ts";
 // 環境変数の読み込み
 await load({ export: true });
 
+
 const TOKEN = Deno.env.get("DISCORD_TOKEN");
 const TARGET_CHANNEL_ID = Deno.env.get("TARGET_CHANNEL_ID");
 
